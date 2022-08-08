1f65a8583501c3aa81196727bc12fb361a6d5565
Jack Hughes <Jack.Hughes@softwire.com>
2022-08-08 15:28:45 +0100

    program now prints 'fizz' instead of multiples of 3

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,3 +1,8 @@
-for (i = 0; i < 101; i++) {
-    console.log(i);
+for (i = 1; i < 101; i++) {
+    if (i % 3 == 0) {
+        console.log("fizz");
+    }
+    else {
+        console.log(i);
+    }  
 };
