a41eb851b7aed7358a2db6e860e9681aade7da21
Jack Hughes <Jack.Hughes@softwire.com>
2022-08-09 11:11:06 +0100

    if/else statement reordered for simplicity

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,19 +1,20 @@
 function fizzBuzz() {
     
     // creating a for loop that runs through every number 1 - 100
-    for (i = 1; i < 101; i++) {
-        // if the number is a multiple of 3 AND NOT a multiple of 5: Print "fizz"
-        if (i % 3 == 0 && i % 5 != 0) {
-            console.log("fizz");
-        }
-        // if the number is a multiple of 5 AND NOT a multiple of 3: Print "buzz"
-        else if (i % 5 == 0 && i % 3 != 0) {
-            console.log("buzz");
-        }
+    for (i = 1; i <= 100; i++) {
         // if the number is a multiple of 3 AND a multiple of 5: Print "fizzbuzz"
-        else if (i % 3 == 0 && i % 5 == 0) {
+        if (i % 3 == 0 && i % 5 == 0) {
             console.log("fizzbuzz");
         }
+        // if the number is a multiple of 3: Print "fizz"
+        else if (i % 3 == 0) {
+            console.log("fizz");
+        }
+        // if the number is a multiple of 5: Print "buzz"
+        else if (i % 5 == 0) {
+            console.log("buzz");
+        }
+        
         // for all other numbers just print the number
         else {
             console.log(i);
