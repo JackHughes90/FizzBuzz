475ed5cce52b1fc50d953fbd8d48fa0a43ed856c
Jack Hughes <Jack.Hughes@softwire.com>
2022-08-09 10:41:39 +0100

    Explanitory comments added and encased in function

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,16 +1,23 @@
-for (i = 1; i < 101; i++) {
-        
-    if (i % 3 == 0 && i % 5 != 0) {
-        console.log("fizz");
-    }
-    else if (i % 5 == 0 && i % 3 != 0) {
-        console.log("buzz");
-    }
-    else if (i % 3 == 0 && i % 5 == 0) {
-        console.log("fizzbuzz");
-    }
-    else {
-        console.log(i);
-    } 
-    
+function fizzBuzz() {
+    // creating a for loop that runs through every number 1 - 100
+    for (i = 1; i < 101; i++) {
+        // if the number is a multiple of 3 AND NOT a multiple of 5: Print "fizz"
+        if (i % 3 == 0 && i % 5 != 0) {
+            console.log("fizz");
+        }
+        // if the number is a multiple of 5 AND NOT a multiple of 3: Print "buzz"
+        else if (i % 5 == 0 && i % 3 != 0) {
+            console.log("buzz");
+        }
+        // if the number is a multiple of 3 AND a multiple of 5: Print "fizzbuzz"
+        else if (i % 3 == 0 && i % 5 == 0) {
+            console.log("fizzbuzz");
+        }
+        // for all other numbers just print the number
+        else {
+            console.log(i);
+        }
+    };
 };
+// Run program
+fizzBuzz();
